7600d39a479e586d4dd65a885c1a3a35d8f8cde8
Noahjundurian <106822871+Noahjundurian@users.noreply.github.com>
2023-06-06 12:30:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+
+
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.siz(300, 300);
+    video.hide();
+
+
+
+
+}
+function draw(){
+
+
+}
